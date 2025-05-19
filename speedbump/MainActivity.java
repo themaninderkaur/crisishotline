4117e9a470be82051541c6c6dc77e275fba8df66
Maninder Kaur <75804314+themaninderkaur@users.noreply.github.com>
2025-05-19 13:12:10 -0400

    import app libraries

--- speedbump/MainActivity.java
+++ speedbump/MainActivity.java
@@ -1,1 +1,13 @@
 package com.example.silverspeedbumps;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.LinearLayout;
